5ad419bf48f16ff0936e009f154473f927b11df5
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-06-12 11:42:25 +0530

    oops.. i keep missing things :expressionless:

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const cv = require('../src/index');
 
 describe('testing current-version', () => {
-	it('should return 0.1.4 while checking', done => {
+	it('should return 1.0.0 while checking', done => {
 		expect(cv().version).to.equal('1.0.0');
 		done();
 	});
