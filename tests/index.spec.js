160e211d051808a1f0a5eea8fd6dd6715a26f10a
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-06-12 11:30:45 +0530

    adjusting tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -5,7 +5,7 @@ const cv = require('../src/index');
 
 describe('testing current-version', () => {
 	it('should return 0.1.4 while checking', done => {
-		expect(cv().version).to.equal('0.1.4');
+		expect(cv().version).to.equal('1.0.0');
 		done();
 	});
 });
